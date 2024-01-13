29ca0477caaa25ed18f67aac905e2a41556c4506
Anish <engineeranish10000@gmail.com>
2024-01-13 11:46:14 +0530

    create vote model

--- app/models.py
+++ app/models.py
@@ -22,4 +22,8 @@ class User(Base):
     email = Column(String,nullable=False,unique=True)
     password = Column(String,nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),nullable=False,server_default=text('now()'))
-    
+
+class Vote(Base):
+    __tablename__ = "votes"
+    post_id = Column(Integer,ForeignKey("posts.id",ondelete="CASCADE"),primary_key=True)
+    user_id = Column(Integer,ForeignKey("users.id",ondelete="CASCADE"),primary_key=True)
